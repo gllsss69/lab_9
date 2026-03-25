691e55a5ae8a40de870a287877eb1bc782a56a4d
gllsss69 <maksympetyk08@gmail.com>
2026-03-25 20:57:56 +0200

    Додано зберігання в json

--- lab_9/Form1.Designer.cs
+++ lab_9/Form1.Designer.cs
@@ -2,15 +2,20 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private System.Windows.Forms.CheckBox chkUseFile;
+        private System.Windows.Forms.GroupBox groupBoxTransport;
+        private System.Windows.Forms.RadioButton radioBus;
+        private System.Windows.Forms.RadioButton radioTrolley;
+        private System.Windows.Forms.RadioButton radioMetro;
+        private System.Windows.Forms.Label lblTransportManual;
+        private System.Windows.Forms.TextBox txtTransport;
+        private System.Windows.Forms.CheckBox chkPrivilegedManual;
+        private System.Windows.Forms.CheckBox chkPrivileged;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,123 +27,162 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.lblResult = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.chkUseFile = new System.Windows.Forms.CheckBox();
+            this.groupBoxTransport = new System.Windows.Forms.GroupBox();
+            this.radioMetro = new System.Windows.Forms.RadioButton();
+            this.radioTrolley = new System.Windows.Forms.RadioButton();
+            this.radioBus = new System.Windows.Forms.RadioButton();
+            this.lblTransportManual = new System.Windows.Forms.Label();
+            this.txtTransport = new System.Windows.Forms.TextBox();
+            this.chkPrivilegedManual = new System.Windows.Forms.CheckBox();
             this.chkPrivileged = new System.Windows.Forms.CheckBox();
             this.btnCalculate = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioBus = new System.Windows.Forms.RadioButton();
-            this.radioTrolley = new System.Windows.Forms.RadioButton();
-            this.radioMetro = new System.Windows.Forms.RadioButton();
-            this.groupBox1.SuspendLayout();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.groupBoxTransport.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lblResult
+            // chkUseFile
             // 
-            this.lblResult.AutoSize = true;
-            this.lblResult.Location = new System.Drawing.Point(135, 258);
-            this.lblResult.Name = "lblResult";
-            this.lblResult.Size = new System.Drawing.Size(80, 16);
-            this.lblResult.TabIndex = 0;
-            this.lblResult.Text = "Результат:";
+            this.chkUseFile.AutoSize = true;
+            this.chkUseFile.Location = new System.Drawing.Point(12, 12);
+            this.chkUseFile.Name = "chkUseFile";
+            this.chkUseFile.Size = new System.Drawing.Size(100, 17);
+            this.chkUseFile.TabIndex = 0;
+            this.chkUseFile.Text = "Читати з файлу";
+            this.chkUseFile.UseVisualStyleBackColor = true;
+            this.chkUseFile.CheckedChanged += new System.EventHandler(this.chkUseFile_CheckedChanged);
             // 
-            // chkPrivileged
+            // groupBoxTransport
             // 
-            this.chkPrivileged.AutoSize = true;
-            this.chkPrivileged.Location = new System.Drawing.Point(105, 162);
-            this.chkPrivileged.Name = "chkPrivileged";
-            this.chkPrivileged.Size = new System.Drawing.Size(231, 20);
-            this.chkPrivileged.TabIndex = 1;
-            this.chkPrivileged.Text = "Пільговий квиток (50% знижка)";
-            this.chkPrivileged.UseVisualStyleBackColor = true;
-            // 
-            // btnCalculate
-            // 
-            this.btnCalculate.Location = new System.Drawing.Point(138, 188);
-            this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(118, 44);
-            this.btnCalculate.TabIndex = 2;
-            this.btnCalculate.Text = "Розрахувати вартість";
-            this.btnCalculate.UseVisualStyleBackColor = true;
-            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.radioMetro);
-            this.groupBox1.Controls.Add(this.radioTrolley);
-            this.groupBox1.Controls.Add(this.radioBus);
-            this.groupBox1.Location = new System.Drawing.Point(105, 56);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Оберіть транспорт:";
-            // 
-            // radioBus
-            // 
-            this.radioBus.AutoSize = true;
-            this.radioBus.Location = new System.Drawing.Point(6, 21);
-            this.radioBus.Name = "radioBus";
-            this.radioBus.Size = new System.Drawing.Size(83, 20);
-            this.radioBus.TabIndex = 4;
-            this.radioBus.TabStop = true;
-            this.radioBus.Text = "Автобус";
-            this.radioBus.UseVisualStyleBackColor = true;
-            // 
-            // radioTrolley
-            // 
-            this.radioTrolley.AutoSize = true;
-            this.radioTrolley.Location = new System.Drawing.Point(6, 40);
-            this.radioTrolley.Name = "radioTrolley";
-            this.radioTrolley.Size = new System.Drawing.Size(100, 20);
-            this.radioTrolley.TabIndex = 5;
-            this.radioTrolley.TabStop = true;
-            this.radioTrolley.Text = "Тролейбус";
-            this.radioTrolley.UseVisualStyleBackColor = true;
+            this.groupBoxTransport.Controls.Add(this.radioMetro);
+            this.groupBoxTransport.Controls.Add(this.radioTrolley);
+            this.groupBoxTransport.Controls.Add(this.radioBus);
+            this.groupBoxTransport.Location = new System.Drawing.Point(12, 40);
+            this.groupBoxTransport.Name = "groupBoxTransport";
+            this.groupBoxTransport.Size = new System.Drawing.Size(384, 160);
+            this.groupBoxTransport.TabIndex = 1;
+            this.groupBoxTransport.TabStop = false;
+            this.groupBoxTransport.Text = "Тип транспорту";
+            this.groupBoxTransport.Visible = false;
             // 
             // radioMetro
             // 
             this.radioMetro.AutoSize = true;
-            this.radioMetro.Location = new System.Drawing.Point(6, 62);
+            this.radioMetro.Location = new System.Drawing.Point(16, 120);
             this.radioMetro.Name = "radioMetro";
-            this.radioMetro.Size = new System.Drawing.Size(70, 20);
-            this.radioMetro.TabIndex = 6;
+            this.radioMetro.Size = new System.Drawing.Size(58, 17);
+            this.radioMetro.TabIndex = 2;
             this.radioMetro.TabStop = true;
-            this.radioMetro.Text = "Метро";
+            this.radioMetro.Text = "Metro";
             this.radioMetro.UseVisualStyleBackColor = true;
+            // 
+            // radioTrolley
+            // 
+            this.radioTrolley.AutoSize = true;
+            this.radioTrolley.Location = new System.Drawing.Point(16, 80);
+            this.radioTrolley.Name = "radioTrolley";
+            this.radioTrolley.Size = new System.Drawing.Size(84, 17);
+            this.radioTrolley.TabIndex = 1;
+            this.radioTrolley.TabStop = true;
+            this.radioTrolley.Text = "Trolleybus";
+            this.radioTrolley.UseVisualStyleBackColor = true;
+            // 
+            // radioBus
+            // 
+            this.radioBus.AutoSize = true;
+            this.radioBus.Location = new System.Drawing.Point(16, 40);
+            this.radioBus.Name = "radioBus";
+            this.radioBus.Size = new System.Drawing.Size(46, 17);
+            this.radioBus.TabIndex = 0;
+            this.radioBus.TabStop = true;
+            this.radioBus.Text = "Bus";
+            this.radioBus.UseVisualStyleBackColor = true;
+            // 
+            // lblTransportManual
+            // 
+            this.lblTransportManual.AutoSize = true;
+            this.lblTransportManual.Location = new System.Drawing.Point(12, 40);
+            this.lblTransportManual.Name = "lblTransportManual";
+            this.lblTransportManual.Size = new System.Drawing.Size(92, 13);
+            this.lblTransportManual.TabIndex = 2;
+            this.lblTransportManual.Text = "Тип транспорту:";
+            // 
+            // txtTransport
+            // 
+            this.txtTransport.Location = new System.Drawing.Point(110, 37);
+            this.txtTransport.Name = "txtTransport";
+            this.txtTransport.Size = new System.Drawing.Size(286, 20);
+            this.txtTransport.TabIndex = 3;
+            // 
+            // chkPrivilegedManual
+            // 
+            this.chkPrivilegedManual.AutoSize = true;
+            this.chkPrivilegedManual.Location = new System.Drawing.Point(12, 65);
+            this.chkPrivilegedManual.Name = "chkPrivilegedManual";
+            this.chkPrivilegedManual.Size = new System.Drawing.Size(143, 17);
+            this.chkPrivilegedManual.TabIndex = 4;
+            this.chkPrivilegedManual.Text = "Пільговий квиток (50%)";
+            this.chkPrivilegedManual.UseVisualStyleBackColor = true;
+            // 
+            // chkPrivileged
+            // 
+            this.chkPrivileged.AutoSize = true;
+            this.chkPrivileged.Location = new System.Drawing.Point(12, 212);
+            this.chkPrivileged.Name = "chkPrivileged";
+            this.chkPrivileged.Size = new System.Drawing.Size(143, 17);
+            this.chkPrivileged.TabIndex = 5;
+            this.chkPrivileged.Text = "Пільговий квиток (50%)";
+            this.chkPrivileged.UseVisualStyleBackColor = true;
+            this.chkPrivileged.Visible = false;
+            this.chkPrivileged.Enabled = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 210);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(384, 36);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Розрахувати вартість";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResult.Location = new System.Drawing.Point(12, 256);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(384, 88);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "Результат: ";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 347);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(412, 360);
+            this.Controls.Add(this.lblResult);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.chkPrivileged);
-            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.chkPrivilegedManual);
+            this.Controls.Add(this.txtTransport);
+            this.Controls.Add(this.lblTransportManual);
+            this.Controls.Add(this.groupBoxTransport);
+            this.Controls.Add(this.chkUseFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+            this.MaximizeBox = true;
             this.Name = "Form1";
             this.Text = "Сервіс розрахунку проїзду";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.groupBoxTransport.ResumeLayout(false);
+            this.groupBoxTransport.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblResult;
-        private System.Windows.Forms.CheckBox chkPrivileged;
-        private System.Windows.Forms.Button btnCalculate;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radioMetro;
-        private System.Windows.Forms.RadioButton radioTrolley;
-        private System.Windows.Forms.RadioButton radioBus;
     }
 }
 
--- lab_9/Form1.cs
+++ lab_9/Form1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows.Forms;
+using System.Text.Json;
 
 namespace lab_9
 {
@@ -10,42 +13,263 @@ namespace lab_9
         private const decimal MetroFare = 6.00m;
         private const decimal PrivilegedDiscount = 0.5m; // 50% off
 
+        private readonly string DataFilePath = Path.Combine(Application.StartupPath, "data.json");
+
         public Form1()
         {
             InitializeComponent();
+            SetInputMode(useFile: chkUseFile.Checked);
+        }
+
+        private void chkUseFile_CheckedChanged(object sender, EventArgs e)
+        {
+            SetInputMode(useFile: chkUseFile.Checked);
+        }
+
+        private void SetInputMode(bool useFile)
+        {
+            groupBoxTransport.Visible = useFile;
+            radioBus.Checked = false;
+            radioTrolley.Checked = false;
+            radioMetro.Checked = false;
+
+            lblTransportManual.Visible = !useFile;
+            txtTransport.Visible = !useFile;
+            chkPrivilegedManual.Visible = !useFile;
+
+            chkPrivileged.Visible = useFile;
+            chkPrivileged.Enabled = useFile;
+            chkPrivileged.Checked = false;
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
+            string transportInput;
+            bool privileged;
 
-            if (!radioBus.Checked && !radioTrolley.Checked && !radioMetro.Checked)
+            if (chkUseFile.Checked)
             {
-                MessageBox.Show("Будь ласка, виберіть тип транспорту.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (!radioBus.Checked && !radioTrolley.Checked && !radioMetro.Checked)
+                {
+                    MessageBox.Show("Будь ласка, виберіть тип транспорту.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                if (!File.Exists(DataFilePath))
+                {
+                    MessageBox.Show("Файл з даними не знайдено: " + DataFilePath, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                try
+                {
+                    ReadFromJsonFile(GetSelectedTransportName(), out transportInput, out privileged);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Не вдалося прочитати файл: " + ex.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                transportInput = (txtTransport.Text ?? string.Empty).Trim();
+                privileged = chkPrivilegedManual.Checked;
+
+                if (string.IsNullOrWhiteSpace(transportInput))
+                {
+                    MessageBox.Show("Будь ласка, введіть тип транспорту у текстове поле.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+            }
+
+            TransportType? transportType = ParseTransport(transportInput);
+            if (!transportType.HasValue)
+            {
+                MessageBox.Show("Невідомий тип транспорту: " + transportInput + Environment.NewLine + "Дозволені значення: Bus, Trolleybus, Metro", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
-            } 
+            }
 
-
-            decimal fare = GetSelectedFare();
-
-            if (chkPrivileged.Checked)
+            decimal fare = GetFareByType(transportType.Value);
+            if (privileged)
             {
                 fare = fare * (1 - PrivilegedDiscount);
             }
 
-            lblResult.Text = $"Результат: {fare:F2} UAH";
+            var dataItem = new FareData
+            {
+                Transport = transportType.Value.ToString(),
+                Privileged = privileged,
+                CalculatedFare = Math.Round(fare, 2)
+            };
+
+            try
+            {
+                SaveDataToJson(dataItem);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не вдалося записати файл: " + ex.Message, "Помилка запису", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            lblResult.Text = $"Результат: {dataItem.CalculatedFare:F2} UAH (Transport: {dataItem.Transport}, Privileged: {dataItem.Privileged})";
+
+            try
+            {
+                string json = File.ReadAllText(DataFilePath);
+                MessageBox.Show("Дані збережено у файл:\n" + DataFilePath + "\n\n" + json, "Збережено", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+            }
         }
 
-        private decimal GetSelectedFare()
+        private enum TransportType
         {
-            if (radioBus.Checked)
-                return BusFare;
-            if (radioTrolley.Checked)
-                return TrolleybusFare;
-            if (radioMetro.Checked)
-                return MetroFare;
+            Bus,
+            Trolleybus,
+            Metro
+        }
 
-            // fallback
-            return BusFare;
+        private TransportType? ParseTransport(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return null;
+
+            string s = input.Trim().ToLowerInvariant();
+            switch (s)
+            {
+                case "bus":
+                case "автобус":
+                    return TransportType.Bus;
+                case "trolleybus":
+                case "trolley":
+                case "тролейбус":
+                    return TransportType.Trolleybus;
+                case "metro":
+                case "метро":
+                    return TransportType.Metro;
+                default:
+                    return null;
+            }
+        }
+
+        private decimal GetFareByType(TransportType t)
+        {
+            switch (t)
+            {
+                case TransportType.Bus: return BusFare;
+                case TransportType.Trolleybus: return TrolleybusFare;
+                case TransportType.Metro: return MetroFare;
+                default: return BusFare;
+            }
+        }
+
+        private string GetSelectedTransportName()
+        {
+            if (radioBus.Checked) return "Bus";
+            if (radioTrolley.Checked) return "Trolleybus";
+            if (radioMetro.Checked) return "Metro";
+            return "Unknown";
+        }
+
+        private void ReadFromJsonFile(string selectedTransport, out string transport, out bool privileged)
+        {
+            transport = selectedTransport;
+            privileged = false;
+
+            string json = File.ReadAllText(DataFilePath);
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            VehiclesContainer container = JsonSerializer.Deserialize<VehiclesContainer>(json, options) ?? new VehiclesContainer();
+
+            if (container.vehicles != null)
+            {
+                foreach (var v in container.vehicles)
+                {
+                    if (string.Equals(v.Transport, selectedTransport, StringComparison.OrdinalIgnoreCase))
+                    {
+                        transport = v.Transport;
+                        privileged = v.Privileged;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void SaveDataToJson(FareData item)
+        {
+            VehiclesContainer container = null;
+            var options = new JsonSerializerOptions { WriteIndented = true, PropertyNameCaseInsensitive = true };
+
+            if (File.Exists(DataFilePath))
+            {
+                try
+                {
+                    string existingJson = File.ReadAllText(DataFilePath);
+                    container = JsonSerializer.Deserialize<VehiclesContainer>(existingJson, options);
+                }
+                catch
+                {
+                    container = null;
+                }
+            }
+
+            if (container == null)
+                container = new VehiclesContainer();
+
+            if (container.vehicles == null)
+                container.vehicles = new List<FareData>();
+
+            bool updated = false;
+            for (int i = 0; i < container.vehicles.Count; i++)
+            {
+                if (string.Equals(container.vehicles[i].Transport, item.Transport, StringComparison.OrdinalIgnoreCase))
+                {
+                    container.vehicles[i].Privileged = item.Privileged;
+                    container.vehicles[i].CalculatedFare = item.CalculatedFare;
+                    updated = true;
+                    break;
+                }
+            }
+            if (!updated)
+                container.vehicles.Add(item);
+
+            EnsureTransportExists(container, TransportType.Bus, BusFare);
+            EnsureTransportExists(container, TransportType.Trolleybus, TrolleybusFare);
+            EnsureTransportExists(container, TransportType.Metro, MetroFare);
+
+            string outJson = JsonSerializer.Serialize(container, options);
+            File.WriteAllText(DataFilePath, outJson);
+        }
+
+        private void EnsureTransportExists(VehiclesContainer container, TransportType type, decimal baseFare)
+        {
+            string name = type.ToString();
+            foreach (var v in container.vehicles)
+            {
+                if (string.Equals(v.Transport, name, StringComparison.OrdinalIgnoreCase))
+                    return;
+            }
+
+            container.vehicles.Add(new FareData
+            {
+                Transport = name,
+                Privileged = false,
+                CalculatedFare = Math.Round(baseFare, 2)
+            });
+        }
+
+        private class VehiclesContainer
+        {
+            public List<FareData> vehicles { get; set; }
+        }
+
+        private class FareData
+        {
+            public string Transport { get; set; }
+            public bool Privileged { get; set; }
+            public decimal CalculatedFare { get; set; }
         }
     }
 }
